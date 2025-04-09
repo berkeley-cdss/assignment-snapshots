31cbf2798eab54f4f5be9bc8dc213f0e3ee65c7b
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-04-09 10:40:26 -0700

    Add NavBar component

--- src/app/client/src/components/NavBar.tsx
+++ src/app/client/src/components/NavBar.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
-function NavBar() {
-  return <div>Unimplemented</div>;
+import { Grid, Typography } from '@mui/material';
+
+function NavBar({ studentName, studentEmail, studentId, assignmentName }) {
+  // TODO fix spacing
+  return (
+    <Typography align="left">Assignment: {assignmentName}</Typography>
+
+    // <Grid container spacing={2} alignItems="center">
+    //   <Grid size={{ xs: 4 }}>
+    //     <Typography align="left">Assignment: {assignmentName}</Typography>
+    //   </Grid>
+    //   <Grid size={{ xs: 4 }}>
+    //     {/* Center Empty Area */}
+    //   </Grid>
+    //   <Grid size={{ xs: 4 }}>
+    //     <Typography align="right">Student: {studentName} {studentEmail} {studentId}</Typography>
+    //   </Grid>
+    // </Grid>
+  );
 }
 
 export default NavBar;
