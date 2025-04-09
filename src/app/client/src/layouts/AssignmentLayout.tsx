2d19cf26777b314c629c8deb43bff39e55b625b6
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-04-09 10:40:42 -0700

    Add AssignmentLayout

--- src/app/client/src/layouts/AssignmentLayout.tsx
+++ src/app/client/src/layouts/AssignmentLayout.tsx
@@ -3,12 +3,15 @@ import { styled } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import { MenuItem, Select, SelectChangeEvent } from '@mui/material';
 
 import AutograderOutput from '../components/AutograderOutput';
 import FileViewer from '../components/FileViewer';
 import Graphs from '../components/Graphs';
 import NavBar from '../components/NavBar';
 import Timeline from '../components/Timeline';
+import { utils, abstractions, recommend, autograder_output } from './constants.ts';
+import { FormControl, InputLabel } from '@mui/material';
 
 const leftSidebarWidth = 240;
 const rightSidebarWidth = 300;
@@ -35,13 +38,35 @@ const ContentWrapper = styled(Box)({
   flexGrow: 1,
 });
 
+// TODO don't hardcode values and instead fetch from server
+// TODO add dropdown to be able to switch to different files in this submission
+
 function AssignmentLayout() {
+  const [file, setFile] = React.useState('utils.py');
+  const [code, setCode] = React.useState(utils)
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setFile(event.target.value as string);
+    if (event.target.value === "utils.py") {
+      setCode(utils);
+    } else if (event.target.value === "abstractions.py") {
+      setCode(abstractions);
+    } else {
+      setCode(recommend);
+    }
+  };
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <AppBar position="static">
         <Toolbar>
           {/* Top Navigation Area */}
-          <NavBar />
+          <NavBar
+            studentName="Rebecca Dang"
+            studentEmail="rdang@berkeley.edu"
+            studentId={3037279631}
+            assignmentName="Maps"
+          />
         </Toolbar>
       </AppBar>
       <ContentWrapper>
@@ -49,14 +74,35 @@ function AssignmentLayout() {
           {/* Left Sidebar Content Area */}
           <Timeline />
         </LeftSidebar>
-        <MainContent>
+        {/* TODO make width more responsive */}
+        <MainContent style={{ width: '300px' }}>
           {/* Main Content Area */}
-          <FileViewer />
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <h2>File Viewer</h2>
+            <FormControl>
+            <InputLabel id="demo-simple-select-label">File</InputLabel>
+            <Select
+            // TODO fix labelId and id
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={file}
+              label="File"
+              onChange={handleChange}
+            >
+              <MenuItem value="utils.py">utils.py</MenuItem>
+              <MenuItem value="abstractions.py">abstractions.py</MenuItem>
+              <MenuItem value="recommend.py">recommend.py</MenuItem>
+            </Select>
+            </FormControl>
+
+          </div>
+
+          <FileViewer code={code} language="python" />
           <Toolbar /> {/* Spacer to offset content below AppBar */}
         </MainContent>
         <RightSidebar sx={{ display: { xs: 'none', sm: 'block' } }}>
           {/* Right Sidebar Content Area */}
-          <AutograderOutput />
+          <AutograderOutput text={autograder_output} />
           <Graphs />
         </RightSidebar>
       </ContentWrapper>
