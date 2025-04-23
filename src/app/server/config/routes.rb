a5a522b003e1b0f1228aca3d4277414a3a67799e
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-04-23 10:24:27 -0700

    Generate FilesController and add GET method to routes

--- src/app/server/config/routes.rb
+++ src/app/server/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # TODO namespace the routes, e.g. prefix with /api?
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/files/:okpy_endpoint/:assignment/:student_id/:backup_id/:file_name", to: "files#show", constraints: { file_name: /.+\..+/ }
 end
---
+++ src/app/server/test/controllers/files_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class FilesControllerTest < ActionDispatch::IntegrationTest
+  # TODO write tests here
+  # test "the truth" do
+  #   assert true
+  # end
+end
