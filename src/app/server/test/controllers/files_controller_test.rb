54c94aac40b3fabf95ccf5301bc01175b4aade38
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-09-18 15:07:16 -0700

    Update files controller unit tests to use mocks

--- src/app/server/test/controllers/files_controller_test.rb
+++ src/app/server/test/controllers/files_controller_test.rb
@@ -1,26 +1,42 @@
+require "ostruct"
+require 'stringio'
 require "test_helper"
 
+require_relative '../../app/controllers/files_controller'
+
 class FilesControllerTest < ActionDispatch::IntegrationTest
-  route_prefix = "/files/a/b/c/d"
-  known_files = [ "data_processor.py", "web_scraper.py", "game_logic.py", "autograder_output.txt" ]
-  file_contents = [ Constants::DATA_PROCESSOR, Constants::WEB_SCRAPER, Constants::GAME_LOGIC, Constants::AUTOGRADER_OUTPUT ]
+  raw_okpy_endpoint = "cal-cs88-sp25"
+  assignment = "maps"
+  student_id = "abc123"
+  backup_id = "def456"
+  route_prefix = "/files/#{raw_okpy_endpoint}/#{assignment}/#{student_id}/#{backup_id}"
+  unknown_file = "unknown.txt"
+  known_files = ["utils.py", "abstractions.py", "recommend.py", "autograder_output.txt"]
+  file_contents = ["foo", "bar", "baz", "qux"]
 
   test "should respond with 404 Not Found if file name unknown" do
-    get "#{route_prefix}/unknown.txt"
+    # Mock AWS S3 client
+    Aws::S3::Client.any_instance
+      .stubs(:get_object)
+      .with(bucket: S3_BUCKET_NAME, key: "cal/cs88/sp25/#{assignment}/#{student_id}/#{backup_id}/#{unknown_file}")
+      .raises(Aws::S3::Errors::NoSuchKey.new(nil, 'The specified key does not exist.'))
+
+    get "#{route_prefix}/#{unknown_file}"
     assert_response :missing
   end
 
-  test "should respond with 200 OK and correct response body if file name is known" do
+  test "should respond with 200 OK and file contents if file name is known" do
     for i in 0...known_files.size
+      # Mock AWS S3 client
+      Aws::S3::Client.any_instance
+        .stubs(:get_object)
+        .with(bucket: S3_BUCKET_NAME, key: "cal/cs88/sp25/#{assignment}/#{student_id}/#{backup_id}/#{known_files[i]}")
+        .returns(OpenStruct.new(body: StringIO.new(file_contents[i])))
+
       get "#{route_prefix}/#{known_files[i]}"
 
       assert_response :success
-
-      assert_equal("a", @response.parsed_body["okpy_endpoint"])
-      assert_equal("b", @response.parsed_body["assignment"])
-      assert_equal("c", @response.parsed_body["student_id"])
-      assert_equal("d", @response.parsed_body["backup_id"])
-      assert_equal(file_contents[i], @response.parsed_body["file_contents"])
+      assert_equal(@response.parsed_body["file_contents"], file_contents[i])
     end
   end
 end
