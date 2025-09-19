a65edd7ef6f6bda006350ff9cf95b29a58ee3c06
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-09-19 09:44:59 -0700

    Add Login page

---
+++ src/app/client/src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Link } from "react-router";
+import { Button } from '@mui/material';
+
+function Login() {
+  return (
+    <div style={{padding: '1rem'}}>
+        <div style={{marginBottom: '1rem'}}>Login with your @berkeley.edu email.</div>
+        <Link to="/courses"><Button variant="contained">Login with Google</Button></Link>
+    </div>
+  );
+}
+
+export default Login;
