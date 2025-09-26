54e27ba0b0e60d079a1c785d37b84022e2450b1f
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-09-26 09:13:21 -0700

    Add /api/files/... route

--- src/test-react-on-rails/config/routes.rb
+++ src/test-react-on-rails/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   # TODO define API routes in separate namespace
   namespace :api, defaults: { format: :json } do
-
+    get "files/:okpy_endpoint/:assignment/:student_id/:backup_id/:file_name", to: "files#show", constraints: { file_name: /.+\..+/ }
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
