29e9763f6fc61f2024c8bcaf323931d644aa81e6
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-09-26 13:20:49 -0700

    Migrate Courses db and seed it with fake values

--- src/snapshots-app/app/controllers/api/courses_controller.rb
+++ src/snapshots-app/app/controllers/api/courses_controller.rb
@@ -1,27 +1,9 @@
 class Api::CoursesController < ApplicationController
   def show
     # TODO retrieve from database based on user id; for now we hardcode
+    # if user id not found, then 404?
     user_id = params[:user_id]
-    courses = [
-      {
-        :id => 1,
-        :course => "CS 61A",
-        :name => "Structure and Interpretation of Computer Programs",
-        :term => "Fall 2025"
-      },
-      {
-        :id => 2,
-        :course => "DATA C88C",
-        :name => "Computational Structures in Data Science",
-        :term => "Spring 2025"
-      },
-      {
-        :id => 3,
-        :course => "CS 61B",
-        :name => "Data Structures",
-        :term => "Fall 2023"
-      },
-    ]
+    courses = Course.all
     render json: { "user_id": user_id, "courses": courses }
   end
 end
