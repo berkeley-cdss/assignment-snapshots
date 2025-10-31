19d79bebaaa59b159743673d64adefc5d1c8e214
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-10-30 22:52:49 -0700

    rails db:migrate

--- src/snapshots-app/db/schema.rb
+++ src/snapshots-app/db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_31_051550) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_31_054803) do
   create_table "backup_metadata", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "dept"
+    t.string "code"
+    t.string "name"
+    t.integer "term"
+    t.integer "year"
+    t.string "okpy_endpoint"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["okpy_endpoint"], name: "index_courses_on_okpy_endpoint", unique: true
   end
 
   create_table "okpy_messages", force: :cascade do |t|
