bfcfb418c83b9e617473afb809057b82b8577adc
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-10-30 23:17:36 -0700

    Add validations for User model

--- src/snapshots-app/app/models/user.rb
+++ src/snapshots-app/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :email_hash, presence: true, uniqueness: true
+  validates :student_id, presence: true, uniqueness: true
 end
