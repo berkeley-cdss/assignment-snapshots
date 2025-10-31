2cccf606352d3ece8edbf2dcc964a1860dced03e
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-10-30 23:06:22 -0700

    Add another unique index on courses

--- src/snapshots-app/app/models/course.rb
+++ src/snapshots-app/app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
   enum :term, { winter: 0, spring: 1, summer: 2, fall: 3 }
+  validates :dept, uniqueness: { scope: [:code, :name, :term, :year],
+                                 message: "A course with this exact department, code, name, term, and year already exists." }
 end
