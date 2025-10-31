9821fe912850b9f32bf76f553821a73d6693b2ee
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-10-31 00:41:48 -0700

    Add `GET /assignments/:course_id` endpoint implementation

--- src/snapshots-app/app/controllers/api/assignments_controller.rb
+++ src/snapshots-app/app/controllers/api/assignments_controller.rb
@@ -1,4 +1,11 @@
 class Api::AssignmentsController < ApplicationController
   def show
+    course_id = params[:course_id].to_i
+    course = Course.find_by(id: course_id)
+    if course.nil?
+      render json: { "error": "Course ID #{course_id} not found" }, status: :not_found
+    else
+      render json: { "course_id": course_id, "assignments": course.assignments }, status: :ok
+    end
   end
 end
