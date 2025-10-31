9c19eb1781ca8ca6cbcf010d38d40d2af844fc4f
Rebecca Dang <35876322+phrdang@users.noreply.github.com>
2025-10-31 07:42:49 -0700

    Setup Jotai for state management (#27)
    * npm i jotai
    
    * Add basic userAtom and test that it works
    
    * Setup some more atoms in atoms.js

--- src/snapshots-app/client/bundles/pages/Login.jsx
+++ src/snapshots-app/client/bundles/pages/Login.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import { Link } from "react-router";
 import { Button } from "@mui/material";
+import { useAtom } from "jotai";
+
+import { userAtom } from "../state/atoms";
 
 function Login() {
+  const [user, setUser] = useAtom(userAtom);
+
   return (
     <div style={{ padding: "1rem" }}>
       <div style={{ marginBottom: "1rem" }}>
-        Login with your @berkeley.edu email.
+        Login with your @berkeley.edu email. User is: {user}
       </div>
       <Link to="/courses">
         <Button variant="contained">Login with Google</Button>
