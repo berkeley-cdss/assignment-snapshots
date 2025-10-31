27466fd3dcb63499c86d5642659531d600722cea
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-10-30 23:22:02 -0700

    Run rails db:migrate

--- src/snapshots-app/db/schema.rb
+++ src/snapshots-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_31_060718) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_31_061821) do
   create_table "assignments", force: :cascade do |t|
     t.string "name"
     t.date "due_date"
@@ -39,10 +39,35 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_31_060718) do
     t.index ["okpy_endpoint"], name: "index_courses_on_okpy_endpoint", unique: true
   end
 
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_enrollments_on_course_id"
+    t.index ["user_id", "course_id"], name: "index_enrollments_on_user_id_and_course_id", unique: true
+    t.index ["user_id"], name: "index_enrollments_on_user_id"
+  end
+
   create_table "okpy_messages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "email_hash"
+    t.integer "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email_hash"], name: "index_users_on_email_hash", unique: true
+    t.index ["student_id"], name: "index_users_on_student_id", unique: true
+  end
+
   add_foreign_key "assignments", "courses"
+  add_foreign_key "enrollments", "courses"
+  add_foreign_key "enrollments", "users"
 end
