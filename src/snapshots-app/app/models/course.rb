555e69b29fb6299919fdb7d9e682ac52883f93a1
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-10-31 01:55:30 -0700

    Add /submissions/:course_id/:assignment_id implementation and update Course model associations

--- src/snapshots-app/app/models/course.rb
+++ src/snapshots-app/app/models/course.rb
@@ -3,4 +3,10 @@ class Course < ApplicationRecord
   validates :dept, uniqueness: { scope: [ :code, :name, :term, :year ],
                                  message: "A course with this exact department, code, name, term, and year already exists." }
   has_many :assignments
+
+  has_many :enrollments
+  has_many :students, through: :enrollments, source: :user
+
+  has_many :staff_memberships
+  has_many :staff_members, through: :staff_memberships, source: :user
 end
