46123c0c657263aa30716755e330c9ac78868c5d
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-10-30 23:25:53 -0700

    Run rails db:migrate

--- src/snapshots-app/db/schema.rb
+++ src/snapshots-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_31_061821) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_31_062234) do
   create_table "assignments", force: :cascade do |t|
     t.string "name"
     t.date "due_date"
@@ -54,6 +54,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_31_061821) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "staff_memberships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_staff_memberships_on_course_id"
+    t.index ["user_id", "course_id"], name: "index_staff_memberships_on_user_id_and_course_id", unique: true
+    t.index ["user_id"], name: "index_staff_memberships_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -70,4 +80,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_31_061821) do
   add_foreign_key "assignments", "courses"
   add_foreign_key "enrollments", "courses"
   add_foreign_key "enrollments", "users"
+  add_foreign_key "staff_memberships", "courses"
+  add_foreign_key "staff_memberships", "users"
 end
