b72400b37d80af05a2acd8ecce20888e5a4533df
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-10-31 07:25:55 -0700

    Add lint errors API endpoint implementation

--- src/snapshots-app/app/controllers/api/lint_errors_controller.rb
+++ src/snapshots-app/app/controllers/api/lint_errors_controller.rb
@@ -1,4 +1,19 @@
 class Api::LintErrorsController < ApplicationController
+  def get_file_contents_location(params)
+    # NOTE: we assume the okpy endpoint is passed in with - as the separator since / is reserved
+    okpy_endpoint_parsed = params[:okpy_endpoint].gsub("-", "/")
+    "#{okpy_endpoint_parsed}/#{params[:assignment]}/#{params[:student_id]}/#{params[:backup_id]}/#{params[:file_name]}"
+  end
+
   def show
+    file_contents_location = get_file_contents_location(params)
+
+    lint_errors = LintError.where(file_contents_location: file_contents_location)
+
+    if lint_errors.exists?
+      render json: { "lint_errors": lint_errors.as_json }, status: :ok
+    else
+      render json: { "lint_errors": [] }, status: :ok
+    end
   end
 end
