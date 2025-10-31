2a5176865bfef0d4227f81aff3af2aea741724ef
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-10-30 23:21:49 -0700

    Add foreign key relationships between enrollments, users, and courses

--- src/snapshots-app/app/models/user.rb
+++ src/snapshots-app/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :email_hash, presence: true, uniqueness: true
   validates :student_id, presence: true, uniqueness: true
+
+  has_many :enrollments
+  has_many :courses, through: :enrollments
 end
--- src/snapshots-app/db/migrate/20251031061821_create_enrollments.rb
+++ src/snapshots-app/db/migrate/20251031061821_create_enrollments.rb
@@ -1,7 +1,14 @@
 class CreateEnrollments < ActiveRecord::Migration[8.0]
   def change
     create_table :enrollments do |t|
+      # Creates column user_id
+      t.references :user, null: false, foreign_key: true
+
+      # Creates column course_id
+      t.references :course, null: false, foreign_key: true
+
       t.timestamps
     end
+    add_index :enrollments, [:user_id, :course_id], unique: true
   end
 end
