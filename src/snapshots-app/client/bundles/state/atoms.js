7a439b8a02cac08dc52b105bcaf2248f31f88bfd
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-11-13 18:08:29 -0800

    Remove unused atoms

--- src/snapshots-app/client/bundles/state/atoms.js
+++ src/snapshots-app/client/bundles/state/atoms.js
@@ -13,13 +13,5 @@ export const assignmentsAtom = atom([]);
 // array of json objects representing all students who submitted for the selected assignment
 export const studentsAtom = atom([]);
 
-// json object representing course info
-// TODO delete this
-export const selectedCourseAtom = atom(null);
-
-// json object representing assignment info
-// TODO delete this
-export const selectedAssignmentAtom = atom(null);
-
 export const backupsAtom = atom([]);
 export const selectedBackupAtom = atom(null);
