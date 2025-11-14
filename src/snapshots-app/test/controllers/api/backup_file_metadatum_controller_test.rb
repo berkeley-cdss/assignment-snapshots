d7d0bb85b6d490909f540b37de5b587d055516ae
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-11-14 14:30:38 -0800

    Comment out unit tests for now

--- src/snapshots-app/test/controllers/api/backup_file_metadatum_controller_test.rb
+++ src/snapshots-app/test/controllers/api/backup_file_metadatum_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class Api::BackupFileMetadatumControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get api_backup_file_metadatum_show_url
-    assert_response :success
+    # TODO write tests
+    # get api_backup_file_metadatum_show_url
+    # assert_response :success
   end
 end
