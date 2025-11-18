9b5b44a46c290141eb7f431f59de6af8590eacb6
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-11-18 10:17:05 -0800

    Create UnlockingTestOutputDialog component

---
+++ src/snapshots-app/client/bundles/components/submission/UnlockingTestOuputDialogServer.js
@@ -0,0 +1,5 @@
+import UnlockingTestOutputDialog from "./UnlockingTestOutputDialog";
+// This could be specialized for server rendering
+// For example, if using React-Router, we'd have the SSR setup here.
+
+export default UnlockingTestOutputDialog;
