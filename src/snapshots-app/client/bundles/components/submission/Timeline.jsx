0ef053c30d739451b51db2408b6aeafaab3ce71a
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-11-18 14:16:00 -0800

    Specify unlocking vs coding tests passed/failed in tooltip in Timeline

--- src/snapshots-app/client/bundles/components/submission/Timeline.jsx
+++ src/snapshots-app/client/bundles/components/submission/Timeline.jsx
@@ -8,34 +8,52 @@ import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 import { Tooltip } from "@mui/material";
 
 function TimelineButton({ backup, selected, index, handleBackupSelect }) {
-  const date = new Date(backup.created);
-  const formattedDate = date.toLocaleString("en-US", {
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: true,
-  });
+  function getNumPassedTests() {
+    if (backup.unlock) {
+      return backup.unlock_message_cases
+        .map((umc) => (umc.correct ? 1 : 0))
+        .reduce((a, b) => a + b, 0);
+    } else {
+      return backup.grading_message_questions
+        .map((gmq) => gmq.passed)
+        .reduce((a, b) => a + b, 0);
+    }
+  }
 
-  const numPassedTests = backup.unlock ? null : backup.grading_message_questions
-    .map((gmq) => gmq.passed)
-    .reduce((a, b) => a + b, 0);
-  const numFailedTests = backup.unlock ? null : backup.grading_message_questions
-    .map((gmq) => gmq.failed)
-    .reduce((a, b) => a + b, 0);
-  const numLockedTests = backup.unlock ? null : backup.grading_message_questions
-    .map((gmq) => gmq.locked)
-    .reduce((a, b) => a + b, 0);
+  function getNumFailedTests() {
+    if (backup.unlock) {
+      return backup.unlock_message_cases
+        .map((umc) => (umc.correct ? 0 : 1))
+        .reduce((a, b) => a + b, 0);
+    } else {
+      return backup.grading_message_questions
+        .map((gmq) => gmq.failed)
+        .reduce((a, b) => a + b, 0);
+    }
+  }
+
+  function getNumLockedTests() {
+    if (backup.unlock) {
+      return null;
+    } else {
+      return backup.grading_message_questions
+        .map((gmq) => gmq.locked)
+        .reduce((a, b) => a + b, 0);
+    }
+  }
 
   function getTooltipTitle() {
     if (backup.unlock) {
-      return "Unlocking test";
+      if (numFailedTests === 0) {
+        return `All ${numPassedTests} unlocking tests passed`;
+      } else {
+        return `${numPassedTests} unlocking tests passed, ${numFailedTests} unlocking tests failed`;
+      }
     } else {
       if (numFailedTests === 0 && numLockedTests === 0) {
-        return `All ${numPassedTests} tests passed`;
+        return `All ${numPassedTests} coding tests passed`;
       } else {
-        return `${numPassedTests} tests passed, ${numFailedTests} tests failed, ${numLockedTests} tests locked`;
+        return `${numPassedTests} coding tests passed, ${numFailedTests} coding tests failed, ${numLockedTests} coding tests locked`;
       }
     }
   }
@@ -51,6 +69,19 @@ function TimelineButton({ backup, selected, index, handleBackupSelect }) {
       }
     }
   }
+
+  const date = new Date(backup.created);
+  const formattedDate = date.toLocaleString("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+  const numPassedTests = getNumPassedTests();
+  const numFailedTests = getNumFailedTests();
+  const numLockedTests = getNumLockedTests();
 
   return (
     <Tooltip title={getTooltipTitle()} placement="right">
