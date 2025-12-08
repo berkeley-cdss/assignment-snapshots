feddc1a96c7d8d28fa74d50949cd6d21830f764d
Rebecca Dang <rdang@berkeley.edu>
2025-12-08 11:52:44 -0800

    Add back autogenerated id for LintError model since I removed the uniqueness constraint but then that messed up the test fixtures

--- src/snapshots-app/db/migrate/20251031140301_create_lint_errors.rb
+++ src/snapshots-app/db/migrate/20251031140301_create_lint_errors.rb
@@ -1,6 +1,6 @@
 class CreateLintErrors < ActiveRecord::Migration[8.0]
   def change
-    create_table :lint_errors, id: false do |t|
+    create_table :lint_errors do |t|
       t.string :file_contents_location, null: false
       t.integer :line_number, null: false
       t.string :message, null: false
