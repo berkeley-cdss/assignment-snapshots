0ce719aff6468e47325f6dc79e4fcb1b9e5e4e7f
Rebecca Dang <35876322+phrdang@users.noreply.github.com>
2025-12-08 11:55:08 -0800

    Setup for CS 61A observations/interviews (#51)
    * Run backups script for 61A fa25 student data
    
    * Install faker gem
    
    * Remove constraints in lint error and unlock message case rails models to match modifications to backups script db schema
    
    * Update backups and backup file metadata controllers to only include backups that are of type grading or unlock
    
    * Fix newlines not showing up in unlocking test output dialog
    
    * Update seeds.rb to use 61A data
    
    * Update brakeman.ignore
    
    * rubocop --autocorrect
    
    * Add openssl 3.3.1 to Gemfile
    
    * Update test fixtures for analytics messages and backup metadata so tests work
    
    * Fix more fixtures
    
    * Add back autogenerated id for LintError model since I removed the uniqueness constraint but then that messed up the test fixtures

--- src/snapshots-app/db/seeds.rb
+++ src/snapshots-app/db/seeds.rb
@@ -8,6 +8,10 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+require 'faker'
+
+Faker::Config.random = Random.new(42)
+
 # Delete existing data (order matters due to foreign key constraints)
 AssignmentProblem.delete_all
 AssignmentFile.delete_all
@@ -18,32 +22,70 @@ Course.delete_all
 User.delete_all
 
 user = {
-  first_name: "Rebecca",
-  last_name: "Dang",
-  email: "rdang@berkeley.edu",
+  first_name: "Oski",
+  last_name: "Bear",
+  email: "oski@berkeley.edu",
   email_hash: "abc123",
-  student_id: 3037279631
+  student_id: 3030000000
 }
 
 courses = [
+  # {
+  #   dept: "DATA",
+  #   code: "C88C",
+  #   name: "Computational Structures in Data Science",
+  #   term: 1, # spring
+  #   year: 2025,
+  #   okpy_endpoint: "cal/cs88/sp25"
+  # }
   {
-    dept: "DATA",
-    code: "C88C",
-    name: "Computational Structures in Data Science",
-    term: 1, # spring
+    dept: "CS",
+    code: "61A",
+    name: "Structure and Interpretation of Computer Programs",
+    term: 3, # fall
     year: 2025,
-    okpy_endpoint: "cal/cs88/sp25"
+    okpy_endpoint: "cal/cs61a/fa25"
   }
 ]
 
 assignments = [
+  # {
+  #   name: "Maps",
+  #   due_date: "2025-03-18 23:59:00",
+  #   okpy_endpoint: "maps",
+  #   files: [ "utils.py", "abstractions.py", "recommend.py" ],
+  #   problems: [
+  #               "Problem 0 Utils",
+  #               "Problem 1",
+  #               "Problem 2",
+  #               "Problem 3",
+  #               "Problem 4",
+  #               "Problem 5",
+  #               "Problem 6",
+  #               "Problem 7",
+  #               "Problem 8",
+  #               "Problem 9",
+  #               "Problem 10"
+  #           ]
+  #   # course: data c88c
+  # },
+  # {
+  #   name: "Lab 0",
+  #   due_date: "2025-01-31 23:59:00",
+  #   okpy_endpoint: "lab00",
+  #   files: [ "lab00.py" ],
+  #   problems: [
+  #               "Python Basics",
+  #               "twenty_twenty_five"
+  #           ]
+  #   # course: data c88c
+  # }
   {
-    name: "Maps",
-    due_date: "2025-03-18 23:59:00",
-    okpy_endpoint: "maps",
-    files: [ "utils.py", "abstractions.py", "recommend.py" ],
+    name: "Ants",
+    due_date: "2025-10-23 23:59:00",
+    okpy_endpoint: "ants",
+    files: [ "ants.py" ],
     problems: [
-                "Problem 0 Utils",
                 "Problem 1",
                 "Problem 2",
                 "Problem 3",
@@ -51,49 +93,73 @@ assignments = [
                 "Problem 5",
                 "Problem 6",
                 "Problem 7",
-                "Problem 8",
+                "Problem 8a",
+                "Problem 8b",
+                "Problem 8c",
                 "Problem 9",
-                "Problem 10"
+                "Problem 10",
+                "Problem 11",
+                "Problem 12"
             ]
-    # course: data c88c
-  },
-  {
-    name: "Lab 0",
-    due_date: "2025-01-31 23:59:00",
-    okpy_endpoint: "lab00",
-    files: [ "lab00.py" ],
-    problems: [
-                "Python Basics",
-                "twenty_twenty_five"
-            ]
-    # course: data c88c
+    # course: cs 61a
   }
+]
+
+cs61a_hashes = [
+  "58a8a808",
+  "c71d6456",
+  "efb1a47d",
+  "ceb1d039",
+  "914da4aa",
+  "2bd5306a",
+  "ff8a7b8d",
+  "ab96e782",
+  "a29adbd5",
+  "537ab9c4",
+  "9c092a35",
+  "90cfed97",
+  "620ee0eb",
+  "483b88d6"
 ]
 
 # TODO figure out email vs email_hash
 students = [
-  {
-    first_name: "Alice",
-    last_name: "Jones",
-    email: "010d927c",
-    email_hash: "010d927c",
-    student_id: 123
-  },
-  {
-    first_name: "Bob",
-    last_name: "Dylan",
-    email: "fabec26a",
-    email_hash: "fabec26a",
-    student_id: 456
-  },
-  {
-    first_name: "Charlie",
-    last_name: "Brown",
-    email: "9614c0e2",
-    email_hash: "9614c0e2",
-    student_id: 789
-  }
+  # {
+  #   first_name: "Alice",
+  #   last_name: "Jones",
+  #   email: "010d927c",
+  #   email_hash: "010d927c",
+  #   student_id: 123
+  # },
+  # {
+  #   first_name: "Bob",
+  #   last_name: "Dylan",
+  #   email: "fabec26a",
+  #   email_hash: "fabec26a",
+  #   student_id: 456
+  # },
+  # {
+  #   first_name: "Charlie",
+  #   last_name: "Brown",
+  #   email: "9614c0e2",
+  #   email_hash: "9614c0e2",
+  #   student_id: 789
+  # }
 ]
+
+def create_fake_student(hash)
+  {
+    first_name: Faker::Name.unique.first_name,
+    last_name: Faker::Name.unique.last_name,
+    email: hash,
+    email_hash: hash,
+    student_id: Faker::IdNumber.unique.valid
+  }
+end
+
+cs61a_hashes.each do |hash|
+  students << create_fake_student(hash)
+end
 
 user_record = User.create!(first_name: user[:first_name], last_name: user[:last_name], email: user[:email], email_hash: user[:email_hash], student_id: user[:student_id])
 
