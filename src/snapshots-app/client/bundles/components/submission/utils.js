0a01c48458bd0bc0eedf6b82ee8ea1c240391d6c
Rebecca Dang <rdang@berkeley.edu>
2026-03-02 19:10:11 -0800

    Implement timeline button grouping based on problem number(s) and type (unlock vs. not)

--- src/snapshots-app/client/bundles/components/submission/utils.js
+++ src/snapshots-app/client/bundles/components/submission/utils.js
@@ -14,4 +14,16 @@ function getOkpyCommand(questionCliNames, unlock) {
   }
 }
 
-export { getOkpyCommand };
+const areArraysEqual = (arr1, arr2) => {
+  if (arr1 === null || arr2 === null) {
+    return false;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((element, index) => element === arr2[index]);
+};
+
+export { getOkpyCommand, areArraysEqual };
