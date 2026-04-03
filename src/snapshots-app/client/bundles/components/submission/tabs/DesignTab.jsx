b37c52d632e09942694af0b25921edcb544a9409
Rebecca Dang <rdang@berkeley.edu>
2026-04-03 10:37:08 -0700

    Add skeleton tab components

---
+++ src/snapshots-app/client/bundles/components/submission/tabs/DesignTab.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DesignTab({}) {
+    return <div>DesignTab</div>;
+}
+
+export default DesignTab;
---
+++ src/snapshots-app/client/bundles/layouts/TabsLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TabsLayout({}) {
+    return <div>TabsLayout</div>;
+}
+
+export default TabsLayout;
