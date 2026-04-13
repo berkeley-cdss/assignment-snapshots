530e5f0995140b54c8639b606d1c1ffd3fa35618
Rebecca Dang <rdang@berkeley.edu>
2026-04-13 08:57:54 -0700

    Implement num_backups; misc code cleanup

--- src/snapshots-app/app/controllers/api/summary_statistics_controller.rb
+++ src/snapshots-app/app/controllers/api/summary_statistics_controller.rb
@@ -21,7 +21,7 @@ SCORING = {
 }
 
 class Api::SummaryStatisticsController < ApplicationController
-  def get_latest_backups(course_id, assignment_id)
+  def get_latest_analytics(course_id, assignment_id)
     BackupMetadatum
       .where(
         course: Course.find(course_id).okpy_endpoint,
@@ -29,9 +29,15 @@ class Api::SummaryStatisticsController < ApplicationController
       )
       .group(:student_email)
       .having('MAX(created)')
+      .joins("INNER JOIN analytics_messages ON analytics_messages.backup_id = backup_metadata.backup_id")
+      .select("backup_metadata.*, analytics_messages.*")
   end
 
-  def get_bins(start, last, bin_size)
+  def get_bins(data)
+    start = data.min
+    last = data.max
+    bin_size = (last - start) / 10
+
     bins = []
     current = start
 
@@ -49,9 +55,7 @@ class Api::SummaryStatisticsController < ApplicationController
   end
 
   def get_score_distribution(course_id, assignment_id, user_id, course, student_email)
-    latest_analytics = get_latest_backups(course_id, assignment_id)
-      .joins("INNER JOIN analytics_messages ON analytics_messages.backup_id = backup_metadata.backup_id")
-      .select("backup_metadata.*, analytics_messages.*")
+    latest_analytics = get_latest_analytics(course_id, assignment_id)
 
     student_score = 0
     data = []
@@ -65,7 +69,6 @@ class Api::SummaryStatisticsController < ApplicationController
         end
       end
 
-
       if backup.student_email == student_email
         student_score = score
       end
@@ -73,16 +76,12 @@ class Api::SummaryStatisticsController < ApplicationController
       data << score
     end
 
-    bin_size = (data.max - data.min) / 10
-    bins = get_bins(data.min, data.max, bin_size)
-
-    return { "xLabels": bins, "studentValue": student_score, "data": data }
+    return { "xLabels": get_bins(data), "studentValue": student_score, "data": data }
   end
 
+  # TODO make this more DRY (a lot of repeated code from previous method)
   def get_problems_solved_distribution(course_id, assignment_id, user_id, course, student_email)
-    latest_analytics = get_latest_backups(course_id, assignment_id)
-      .joins("INNER JOIN analytics_messages ON analytics_messages.backup_id = backup_metadata.backup_id")
-      .select("backup_metadata.*, analytics_messages.*")
+    latest_analytics = get_latest_analytics(course_id, assignment_id)
 
     student_solved = 0
     data = []
@@ -96,7 +95,6 @@ class Api::SummaryStatisticsController < ApplicationController
         end
       end
 
-
       if backup.student_email == student_email
         student_solved = solved
       end
@@ -104,15 +102,30 @@ class Api::SummaryStatisticsController < ApplicationController
       data << solved
     end
 
-    bin_size = (data.max - data.min) / 10
-    bins = get_bins(data.min, data.max, bin_size)
-
-    return { "xLabels": bins, "studentValue": student_solved, "data": data }
+    return { "xLabels": get_bins(data), "studentValue": student_solved, "data": data }
   end
 
-  def get_number_of_backups_distribution(course_id, assignment_id, user_id)
-    # TODO implement
-    return {}
+  def get_number_of_backups_distribution(course_id, assignment_id, user_id, student_email)
+    num_backups = BackupMetadatum
+      .where(
+        course: Course.find(course_id).okpy_endpoint,
+        assignment: Assignment.find(assignment_id).okpy_endpoint,
+      )
+      .group(:student_email)
+      .select("student_email, COUNT(*) as num_backups")
+
+    student_num_backups = 0
+    data = []
+
+    num_backups.each do |backup|
+      data << backup.num_backups
+
+      if backup.student_email == student_email
+        student_num_backups = backup.num_backups
+      end
+    end
+
+    return { "xLabels": get_bins(data), "studentValue": student_num_backups, "data": data }
   end
 
   def get_total_time_spent_distribution(course_id, assignment_id, user_id)
@@ -162,7 +175,7 @@ class Api::SummaryStatisticsController < ApplicationController
       "user_id": user_id,
       "score_distribution": get_score_distribution(course_id, assignment_id, user_id, course, student.email),
       "problems_solved_distribution": get_problems_solved_distribution(course_id, assignment_id, user_id, course, student.email),
-      "number_of_backups_distribution": get_number_of_backups_distribution(course_id, assignment_id, user_id),
+      "number_of_backups_distribution": get_number_of_backups_distribution(course_id, assignment_id, user_id, student.email),
       "total_time_spent_distribution": get_total_time_spent_distribution(course_id, assignment_id, user_id),
       "active_time_spent_distribution": get_active_time_spent_distribution(course_id, assignment_id, user_id),
       "lint_errors_distribution": get_lint_errors_distribution(course_id, assignment_id, user_id)
