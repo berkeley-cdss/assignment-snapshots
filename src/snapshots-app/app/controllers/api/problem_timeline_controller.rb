973edf79f0c334c1eef97a09825aeab3a06b723a
Rebecca Dang <rdang@berkeley.edu>
2026-04-14 12:23:26 -0700

    rails generate ProblemTimeline controller

---
+++ src/snapshots-app/app/controllers/api/problem_timeline_controller.rb
@@ -0,0 +1,2 @@
+class Api::ProblemTimelineController < ApplicationController
+end
---
+++ src/snapshots-app/test/controllers/api/problem_timeline_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::ProblemTimelineControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
